d0ead1bdc3e5528620210cb43679fb355cc05fdc
Dimitrii Fominykh <a@webheads.ru>
2025-01-10 05:13:44 +0200

    Скрипты в ините, окно для гарантии

--- include/header_email.php
+++ include/header_email.php
@@ -1,3 +1,60 @@
-<b><a href="tel:+74951468811">+74951468811<span id="bx-cursor-node"> </span></a><br></b>
-<b><a href="mailto:guarantee@white-siberia.ru">guarantee@white-siberia.ru<span id="bx-cursor-node"> </span></a><br>
- Отдел гарантии</b>
+<style>
+    .tooltip-container {
+        position: relative;
+        display: inline-block;
+        margin: 10px;
+        cursor: pointer;
+    }
+
+    .tooltip-content {
+        display: none;
+        position: absolute;
+        top: 100%;
+        left: 0;
+        max-width: 210px;
+        min-height: 100px;
+        background: #333;
+        color: #fff;
+        padding: 10px;
+        border-radius: 4px;
+        white-space: normal;
+        z-index: 999;
+        flex-direction: column;
+        gap: 20px;
+    }
+</style>
+
+<div class="tooltip-container">
+    <b><a href="tel:+74951468811">+74951468811<span id="bx-cursor-node"> </span></a></b>
+    <b>Отдел гарантии</b>
+    <div class="tooltip-content">
+        <b><a href="tel:+74951468811">+74951468811<span id="bx-cursor-node"> </span></a><br></b>
+        <b><a href="tel:+74951468811">+79282427640<span id="bx-cursor-node"> </span></a><br></b>
+        <b><a href="mailto:guarantee@white-siberia.ru">guarantee@white-siberia.ru<span id="bx-cursor-node"> </span></a><br></b>
+        <div class="buttons_block">
+            <span class="btn btn-transparent-border-color animate-load has-ripple"
+                  data-event="jqm"
+                  data-param-form_id="GUARANTEE"
+                  data-name="guarantee">
+                Написать сообщение
+            </span>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(function () {
+        $('.tooltip-container').on('mouseenter', function () {
+            $(this).find('.tooltip-content').css('display', 'flex'); // Показываем окно
+        });
+
+        $('.tooltip-container').on('mouseleave', function () {
+            const tooltip = $(this).find('.tooltip-content');
+
+            // Проверяем, уходит ли курсор с самого окна
+            tooltip.on('mouseleave', function () {
+                $(this).css('display', 'none'); // Закрываем окно при уходе курсора
+            });
+        });
+    });
+</script>
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -349,3 +349,87 @@ function sendCurl($data, $method, $idempotenceKey, $shopId, $secretKey)
     curl_close($curl);
     return json_decode($result, true);
 }
+
+/**
+ * Временный агент для активации услуг для менеджеров
+ * @return string
+ */
+function activateSpecificProducts()
+{
+    if (!\CModule::IncludeModule("iblock")) {
+        return "activateSpecificProducts();";
+    }
+
+    $productIds = [20225, 20226, 20227, 20224];
+    $iblockId = 55;
+
+    $arFilter = [
+        "IBLOCK_ID" => $iblockId,
+        "ID" => $productIds,
+        "ACTIVE" => "N"
+    ];
+
+    $res = CIBlockElement::GetList([], $arFilter, false, false, ["ID"]);
+    while ($element = $res->Fetch()) {
+        $el = new CIBlockElement;
+        $el->Update($element["ID"], ["ACTIVE" => "Y"]);
+    }
+
+    return "activateSpecificProducts();";
+}
+
+/**
+ * Logs a message to Telegram.
+ *
+ * @param mixed $messageText The message to be logged.
+ * @param string|null $groupChatId Optional group chat ID.
+ * @return void
+ */
+function LogTG($messageText, $groupChatId = null)
+{
+    $botToken = "5865641167:AAF55jrqMP0zFAGrU7Bv-1KUDj_7chXsVWc";
+    $defaultChatId = "141079661";
+    $chatId = $groupChatId ? $groupChatId : $defaultChatId;
+
+    $telegramUrl = "https://api.telegram.org/bot".$botToken."/sendMessage";
+    $text = $_SERVER["HTTP_HOST"]. "\n";
+    $text .= print_r($messageText, true);
+    $telegram_params = [
+        'chat_id' => $chatId,
+        'text' => $text,
+    ];
+
+    $options = [
+        'http' => [
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($telegram_params),
+        ],
+    ];
+
+    $context  = stream_context_create($options);
+    file_get_contents($telegramUrl, false, $context);
+}
+
+/**
+ * Удобная принтилка
+ * @param $ar
+ * @param $dark
+ * @param $die
+ * @return string|void
+ */
+function pr($ar, $dark = false, $die = false)
+{
+    global $USER;
+    if (!$USER->IsAdmin()) return "";
+
+    if(!$dark)
+    {
+        echo "<pre style='font-size:11px;line-height:1.2; padding:5px;'>".print_r($ar, 1)."</pre>";
+    }
+    else
+    {
+        echo '<pre style="line-height:1.2; padding:2em;font-size:11px;background: #282c34; color: #61dafb">' .print_r($ar, true).'</pre>';
+    }
+    if($die) die();
+}
